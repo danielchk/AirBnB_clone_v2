1fdc4c1b7447143ad6baffb2db9c758783e8744e
danielchk <hollowdaniel1@gmail.com>
2021-01-27 16:43:33 -0500

    enviromental variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,3 +11,8 @@ app.register_blueprint(app_views)
 def teardown(self):
     """teardown to app"""
     models.storage.close()
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST") or '0.0.0.0'
+    port = getenv("HBNB_API_PORT") or 5000
+    app.run(host=host, port=port, threaded=True)
