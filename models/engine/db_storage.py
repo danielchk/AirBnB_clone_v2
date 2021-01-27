8d124a972cb3046961632b47b9fb4ed53ec72819
danielchk <hollowdaniel1@gmail.com>
2021-01-27 09:48:12 -0500

    task 3

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found"""
+        if cls in classes and id and type(id) == str:
+            dict_objects = self.all(cls)
+            return dict_objects.get(cls + '.' + id, None)
+    
+    def count(self, cls=None):
+        """Number of objects of a class name"""
+        return len(self.all(cls))
