a4585cdc726dff906012b4ac4a2707356b43527b
danielchk <hollowdaniel1@gmail.com>
2021-01-27 17:46:09 -0500

    fixing route of API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """Index file to views"""
 from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def status():
+    """Status route of API v1"""
+    d = {"status": "OK"}
+    return jsonify(d)
