c77b95755ad94af2ff684c59e054b68ea5e8821f
danielchk <hollowdaniel1@gmail.com>
2021-01-28 08:24:16 -0500

    add count to storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,23 @@
 """Status file"""
 from api.v1.views import app_views
 from flask import jsonify
+import models
 
 
 @app_views.route('/status')
 def status():
-    """Status route of API v1"""
+    """Status route to message OK"""
     d = {"status": "OK"}
     return jsonify(d)
+
+
+@app_views.route('/stats')
+def stats():
+    """count the different stats"""
+    c = {"amenities": storage.count("Amenity"),
+         "cities": storage.count("City"),
+         "places": storage.count("Place"),
+         "reviews": storage.count("Review"),
+         "states": storage.count("State"),
+         "users": storage.count("User")}
+    return jsonify(c)
