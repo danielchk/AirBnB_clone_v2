aaf587520630aef0733be98c78f69c7cea3128a5
danielchk <hollowdaniel1@gmail.com>
2021-01-29 10:28:45 -0500

    task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}}
+cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}})
 
 
 @app.teardown_appcontext
