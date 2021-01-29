1a9b1b4ed7e4f05f8d712c8428adb2472bbf4135
danielchk <hollowdaniel1@gmail.com>
2021-01-28 20:42:12 -0500

    fixing 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,37 +2,36 @@
 """ holds class State"""
 from models import storage
 from api.v1.views import app_views
-from models.state import State
+from models.state import City
 from flask import jsonify, abort, request, make_response
 
-
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities', methods=['GET'], strict_slashes=False)
 def get():
     """Return the objects without his id"""
-    st = [obj.to_dict() for obj in storage.all("State").values()]
-    return jsonify(st)
+    ct = [obj.to_dict() for obj in storage.all("City").values()]
+    return jsonify(ct)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_by_id(state_id):
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+def get_by_id(city_id):
     """Return the objects with his id"""
-    obj = storage.get("State", state_id)
+    obj = storage.get("City", city_id)
     if obj is None:
         abort(404)
     return jsonify(obj.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('/states/<state_id>/cities', methods=['DELETE'],
                  strict_slashes=False)
-def delete(state_id):
+def delete(city_id):
     """Delete with the id info"""
-    obj = storage.get("State", state_id)
+    obj = storage.get("City", city_id)
     if obj is None:
         abort(404)
     obj.delete()
     storage.save()
     storage.reload()
-    return make_response(jsonify({}), 200)
+    return jsonify({})
 
 
 @app_views.errorhandler(400)
@@ -40,15 +39,15 @@ def resource_not_found(e):
     return jsonify(error=str(e)), 400
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/api/v1/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create():
-    """Create a state object"""
+    """Create a city object"""
     if not request.get_json():
         return jsonify({'error': 'Not a JSON'}), 400
     if 'name' not in request.get_json():
         return jsonify({'error': 'Missing name'}), 400
     name = request.get_json().get('name')
-    obj = State(name=name)
+    obj = City(name=name)
     obj.save()
     return jsonify(obj.to_dict()), 201
 
